8f01b1621c8a7dbd7dd769214f5e258def68b25f
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-05-20 15:23:29 -0400

    variables

--- src/Patient.java
+++ src/Patient.java
@@ -1,8 +1,11 @@
 public class Patient {
-private String patient_id;
-private String first_name;
-private String last_name;
-private String address;
-private String city;
+    private String patient_id;
+    private String first_name;
+    private String last_name;
+    private String address;
+    private String city;
+    private String porvince;
+    private String postal_code;
+
 
 }
