13a1acf4a1d3bf27e67708bd6fa476bd7aeefd8e
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-05-20 17:36:51 -0400

    comments added

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,13 +1,20 @@
 public class BankAccount {
+
+    //variables
+
     private String account_number;
     private String name;
     private double balance;
+
+    //constructor
 
     public BankAccount(String account_number, String name, double balance) {
         this.account_number = account_number;
         this.name = name;
         this.balance = balance;
     }
+
+//get and set for account number
 
     public String getAccount_number() {
         return account_number;
@@ -17,6 +24,8 @@ public class BankAccount {
         this.account_number = account_number;
     }
 
+// get and set for name
+
     public String getName() {
         return name;
     }
@@ -24,6 +33,7 @@ public class BankAccount {
     public void setName(String name) {
         this.name = name;
     }
+// get and set for balance
 
     public double getBalance() {
         return balance;
@@ -32,14 +42,23 @@ public class BankAccount {
     public void setBalance(double balance) {
         this.balance = balance;
     }
+
+    //deposit money method
+
     public void deposit_money(double deposit_amt){
         if(deposit_amt>0.00)
             balance+=deposit_amt;
     }
+
+    // withdraw money method
+
     public void withdraw_money(double withdraw_amt){
         if(withdraw_amt>0.00&&withdraw_amt<=balance)
             balance-=withdraw_amt;
     }
+
+    //get account information
+
     public String getAccountInfo(){
         return "Account number= "+account_number+"\n"+"Name= "+name+"\n"+"Balance= $"+balance+"\n";
     }
--- src/BankAccountTest.java
+++ src/BankAccountTest.java
@@ -7,6 +7,9 @@ public class BankAccountTest {
         String option=JOptionPane.showInputDialog("Enter D to deposit \nOR\nEnter W to withdraw");
 
         BankAccount customer1=new BankAccount(account_number,name,50.00);
+
+        //if user enter D 
+
         if(option.equals("D")){
             String deposit=JOptionPane.showInputDialog("Enter Deposit Amount");
             double amount= Double.parseDouble(deposit);
@@ -14,6 +17,9 @@ public class BankAccountTest {
             customer1.deposit_money(amount);
             customer1.getAccountInfo();
         }
+
+        //if user enter W
+
         if(option.equals("W")){
             String withdraw=JOptionPane.showInputDialog("Enter Withdraw Amount");
             double amount= Double.parseDouble(withdraw);
@@ -21,6 +27,9 @@ public class BankAccountTest {
             customer1.withdraw_money(amount);
             customer1.getAccountInfo();
         }
+
+        // show result message
+
         String amount_message= customer1.getAccountInfo();
         JOptionPane.showMessageDialog(null,amount_message);
     }
