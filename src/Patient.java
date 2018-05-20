3501372153a8e2d6acf2fb39aa5092738841934d
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-05-20 15:31:57 -0400

    constructor and getter setter methods

--- src/Patient.java
+++ src/Patient.java
@@ -7,5 +7,70 @@ public class Patient {
     private String porvince;
     private String postal_code;
 
+    public Patient(String patient_id, String first_name, String last_name, String address, String city, String porvince, String postal_code) {
+        this.patient_id = patient_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+        this.city = city;
+        this.porvince = porvince;
+        this.postal_code = postal_code;
+    }
 
+    public String getPatient_id() {
+        return patient_id;
+    }
+
+    public void setPatient_id(String patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPorvince() {
+        return porvince;
+    }
+
+    public void setPorvince(String porvince) {
+        this.porvince = porvince;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+    
 }
